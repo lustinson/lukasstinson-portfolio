05d6b20dce337052bd7754dce96c06c46f7a8e1c
Lukas Stinson <stinser01@gmail.com>
2026-05-11 20:55:54 -0700

    robots.ts update

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,12 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        disallow: ['/_next/'],
+      },
+    ],
     sitemap: 'https://lukasstinson.com/sitemap.xml',
   };
 }
